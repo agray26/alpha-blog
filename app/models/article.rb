df4bc99ed7ef1315b81ae8f800ba9c2d7ee75dff
AustinGray <mrdrsirman@gmail.com>
2021-10-16 13:29:53 -0400

    Adding edit action to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 6, maximum: 69}
-    validates :description, presence: true, length: { minimum: 10, maximum: 200}
+    validates :title, presence: true, length: { minimum: 1, maximum: 69}
+    validates :description, presence: true, length: { minimum: 1, maximum: 200}
 end
