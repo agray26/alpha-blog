2ce1a9202f581ace8d5d17dc0db2415e3521abc6
AustinGray <mrdrsirman@gmail.com>
2021-10-21 20:56:50 -0400

    Adding styling with errors because I will fix them later

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ActionController::Base
     def home
+      respond_to do |format|
+        format.html { render :home }
+      end
     end
 
     def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  get 'home/index'
+  root 'home#index'
   get 'about', to: 'pages#about'
   resources :articles
 end
