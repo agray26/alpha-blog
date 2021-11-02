dac6c98e56f89535bb3df8ecaf48b7b52c3a1d93
AustinGray <mrdrsirman@gmail.com>
2021-11-02 19:23:34 -0400

    Clean up layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,9 @@ class UsersController < ActionController::Base
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
+      render 'edit'
     end
   end
 
