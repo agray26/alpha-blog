dd5ba8cba3b84e3d20dfbe5059e0ed4e85306f03
AustinGray <mrdrsirman@gmail.com>
2021-11-09 19:23:22 -0500

    trying to fix migration issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_005054) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-    t.boolean "admin", default: false
-  end
+ActiveRecord::Schema.define(version: 2021_10_22_014537) do
 
 end
